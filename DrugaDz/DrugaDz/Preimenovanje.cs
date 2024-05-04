a361cfa793758249fef3d8353ecfb08284917a95
stankela8 <157402589+stankela8@users.noreply.github.com>
2024-05-04 12:36:27 +0200

    Add files via upload
    Zadaća iz refaktoriranja i preimenovanja.

---
+++ DrugaDz/DrugaDz/Preimenovanje.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrugaDz
+{
+    public class Preimenovanje
+    {
+        //2. zadatak
+        public class Product
+        {
+            public string Name { get; private set; } 
+            public string Price { get; private set; } 
+            public bool InStock { get; set; } 
+
+            public Product(string name, string price)
+            {
+                Name= name;
+                Price = price;
+                InStock = false;
+            }
+        }
+        public class ProductHandler
+        {
+            List<Product> products;
+
+            public ProductHandler(List<Product> inventory) 
+            {
+                products = inventory;
+            }
+            public void RestockProduct(Product product) 
+            {
+                foreach(Product item in  products) 
+                {
+                    if(product==item)
+                    {
+                        item.InStock = true;
+                    }
+                }
+            }
+            public void RemoveSoldOutProducts()
+            {
+                products.RemoveAll(product => product.InStock == false);
+            }
+        }
+        //4. zadatak
+        public class Note
+        {
+            public string Title { get; set; }
+            public string Text { get; set; }
+            public DateTime CreatedAt { get; private set; }
+
+            public Note(string title, string text)
+            {
+                Title = title;
+                Text = text;
+                CreatedAt = DateTime.Now;
+            }
+        }
+        public class NoteCollection
+        {
+            public string Author { get; private set; }
+            public List<Note> notes;
+
+            public NoteCollection(string author)
+            {
+                Author = author;
+                notes = new List<Note>();
+            }
+
+            public void AddNote(Note note)
+            {
+                notes.Add(note);
+            }
+        }
+        //5. zadatak
+        public class Location
+        {
+            public DateTime CreatedAt { get; private set; } 
+            public double Latitude { get; private set; } 
+            public double Longitude { get; private set; } 
+
+            public Location(double latitude, double longitude)
+            {
+                Latitude = latitude;
+                Longitude = longitude;
+                CreatedAt = DateTime.Now;
+            }
+        }
+        public class PathManaging
+        {
+            private List<Location> pathLocations;
+
+            public PathManaging()
+            {
+                pathLocations = new List<Location>();
+            }
+
+            public void AddLocation(Location location)
+            {
+                pathLocations.Add(location);
+            }
+
+            public void RemoveLocation(Location location)
+            {
+                pathLocations.Remove(location);
+            }
+        }
+    }
+}
---
+++ DrugaDz/DrugaDz/Refaktoriranje.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrugaDz
+{
+    public class Refaktoriranje
+    {
+        //2. zadatak
+        public class Average
+        {
+            public List<double> CalculateAverages(List<double[]> arrays)
+            {
+                List<double> averages = new List<double>();
+                foreach (double[] array in arrays) 
+                {
+                    double sum = CalculateSum(array);
+                    double average=sum/array.Length;
+                    averages.Add(average);
+                }
+                return averages;
+            }
+            public double CalculateSum(double[] array)
+            {
+                double sum = 0;
+                foreach (double value in array) 
+                {
+                    sum += value;
+                }
+                return sum;
+            }
+        }
+        //4. zadatak
+        public class UniqueCharacterFinder
+        {
+            public void CountCharOccurence(string text)
+            {
+                var characterCounts = new Dictionary<char,int>();
+                foreach (char character in text)
+                {
+                    if(characterCounts.ContainsKey(character))
+                        characterCounts[character]++;
+                    else
+                        characterCounts.Add(character, 1);
+                }
+            }
+            public List<char> GetUniqueChars (Dictionary<char,int> characterCounts)
+            {
+                List<char> uniqueChars = new List<char>();
+                foreach (var chararacter in characterCounts)
+                {
+                    if (chararacter.Value == 1)
+                        uniqueChars.Add(chararacter.Key);
+                }
+                return uniqueChars;
+            }
+        }
+        //5. zadatak
+        public class PalindromeFinder
+        {
+            public List<string> FindPalindromes(List<string> strings)
+            {
+                if (strings == null)
+                    return new List<string>();
+
+                List<string> palindromes = new List<string>();
+                foreach (string str in strings)
+                {
+                    if (IsPalindrome(str))
+                        palindromes.Add(str);
+                }
+                return palindromes;
+            }
+
+            private bool IsPalindrome(string input)
+            {
+                string cleaned = RemoveSpacesAndToLower(input);
+                string reversed = ReverseString(cleaned);
+                return cleaned.Equals(reversed);
+            }
+
+            private string RemoveSpacesAndToLower(string input)
+            {
+                return input.Replace(" ", "").ToLower();
+            }
+
+            private string ReverseString(string input)
+            {
+                return new string(input.Reverse().ToArray());
+            }
+        }
+    }
+}
