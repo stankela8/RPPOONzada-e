6e77b3b7a006a6f17cb7ca542fe94ca261df2b9c
stankela8 <157402589+stankela8@users.noreply.github.com>
2024-05-19 12:10:50 +0200

    Add files via upload
    Peta zadaća, dekorater

---
+++ PetaZadaca/PetaZadaca/Decorater.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetaZadaca
+{
+    public class Decorater
+    {
+        public interface Coffee
+        {
+            double GetCost();
+            string GetDescription();
+        }
+        public class Espresso : Coffee
+        {
+            public double GetCost()
+            {
+                return 1.99;
+            }
+
+            public string GetDescription()
+            {
+                return "Espresso";
+            }
+        }
+
+        public abstract class CoffeeDecorator : Coffee
+        {
+            protected Coffee coffee;
+
+            public CoffeeDecorator(Coffee coffee)
+            {
+                this.coffee = coffee;
+            }
+
+            public abstract double GetCost();
+            public abstract string GetDescription();
+        }
+
+        public class Milk : CoffeeDecorator
+        {
+            public Milk(Coffee coffee) : base(coffee) { }
+
+            public override double GetCost()
+            {
+                return coffee.GetCost() + 0.5;
+            }
+
+            public override string GetDescription()
+            {
+                return coffee.GetDescription() + ", Milk";
+            }
+        }
+
+        public class Sugar : CoffeeDecorator
+        {
+            public Sugar(Coffee coffee) : base(coffee) { }
+
+            public override double GetCost()
+            {
+                return coffee.GetCost() + 0.2;
+            }
+
+            public override string GetDescription()
+            {
+                return coffee.GetDescription() + ", Sugar";
+            }
+        }
+
+
+    }
+}
---
+++ PetaZadaca/PetaZadaca/Program.cs
@@ -0,0 +1,16 @@
+﻿using static PetaZadaca.Decorater;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Coffee myCoffee = new Espresso();
+        Console.WriteLine($"{myCoffee.GetDescription()} costs {myCoffee.GetCost()}");
+
+        myCoffee = new Milk(myCoffee);
+        Console.WriteLine($"{myCoffee.GetDescription()} costs {myCoffee.GetCost()}");
+
+        myCoffee = new Sugar(myCoffee);
+        Console.WriteLine($"{myCoffee.GetDescription()} costs {myCoffee.GetCost()}");
+    }
+}
